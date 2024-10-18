0a9d5c0f96e1213fa5ffa823b5d4555fdea68cca
Quidsnetwork <quidsnetwork@gmail.com>
2024-10-18 11:47:55 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,14 +1,1 @@
-const mysql = require('mysql2/promise');
 
-// MySQL connection pool
-const pool = mysql.createPool({
-  host: 'srv1508.hstgr.io',
-  user: 'u532021302_q',
-  password: 'gangstaR551@',
-  database: 'u532021302_q',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
-
-module.exports = pool;
